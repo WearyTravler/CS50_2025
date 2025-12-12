a7f859e7c6fa1362a93ecd50fee33424fe15f38b
WearyTravler <limbo_shell@protonmail.com>
2025-12-11 23:02:49 -0600

    changes to mario to get a better score

--- Week1/mario/mario.c
+++ Week1/mario/mario.c
@@ -1,23 +1,36 @@
 #include <stdio.h>
 
-int main() {
+int main(void) {
     int tower_height;
 
-    
-    printf("Welcome to the Super Mario Tower Builder! How tall would you like your tower: ");
-    scanf("%d", &tower_height);
-    
+
+    do {
+        printf("Height: ");
+        if (scanf("%d", &tower_height) != 1) {
+
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF) {}
+            tower_height = 0;
+        }
+    } while (tower_height < 1 || tower_height > 8);
+
+    // Build the tower
     for (int row = 1; row <= tower_height; row++) {
+
         for (int space = 0; space < (tower_height - row); space++) {
             printf(" ");
         }
+
         for (int hash = 0; hash < row; hash++) {
             printf("#");
         }
+
         printf("  ");
+
         for (int hash = 0; hash < row; hash++) {
             printf("#");
         }
+
         printf("\n");
     }
 
